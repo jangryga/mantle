fa041c1a55c73d023ec8b616c5d6adfeefb3a754
jangryga <jangryga@gmail.com>
2023-10-01 11:39:31 +0200

    Init with tao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use tao::event_loop::{EventLoop, ControlFlow};
+use tao::window::WindowBuilder;
+use tao::event::{Event, WindowEvent};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                println!("The close button was pressed; stopping");
+                *control_flow = ControlFlow::Exit;
+            },
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            },
+            Event::RedrawRequested(_) => {
+                println!("redraw requested!");
+            },
+            _ => ()
+        }
+
+    })
 }
